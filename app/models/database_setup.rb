4a18d135052cb9ed4e026e69ffa266ed56922f25
Leigh-ann <leighann.gant@gmail.com>
2018-01-25 20:21:42 +0000

    files cleaned up

--- app/models/database_setup.rb
+++ app/models/database_setup.rb
@@ -4,6 +4,5 @@ require 'dm-postgres-adapter'
 require_relative './link'
 require_relative './tag'
 
-
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
--- config.ru
+++ config.ru
@@ -1,8 +1,6 @@
 require_relative './app/app'
 require_relative './app/models/database_setup'
 
-
 run BookMarkManager
 
 ENV['RACK_ENV'] ||= 'development'
-# database_setup
